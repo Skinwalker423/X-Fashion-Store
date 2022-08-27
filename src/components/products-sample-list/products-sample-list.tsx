09df7e4a41a0a2d244fc6effe0f243a1829e8d84
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-08-26 23:52:05 -0700

    finished TS

---
+++ src/components/products-sample-list/products-sample-list.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ProductCard from "../products-card/product-card-component";
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { selectCategoriesMap, selectIsLoading } from "../../store/categories/categories.selector";
+import { useSelector } from "react-redux";
+import Spinner from "../spinner/spinner-component";
+
+
+import { CategoriesSampleContainer, ProductTitle, ProductsContainer } from "./products-sample-list-styles";
+
+const ProductsSampleList = () => {
+
+    const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectIsLoading);
+
+    const products = Object.keys(categoriesMap).map((title) => {
+        return(
+
+        <CategoriesSampleContainer key={title}>
+            <Link to={title}><ProductTitle>{title}</ProductTitle></Link>
+            <ProductsContainer>
+                {categoriesMap[title].map((item, index) =>{
+                    if(index >= 4){
+                        return null;
+                    }
+                    return(
+                        <ProductCard 
+                        product={item}
+                        key={item.id}
+                        category={title}
+                        />
+                        )
+                    })}
+            </ProductsContainer>
+        </CategoriesSampleContainer>
+        )
+    })
+
+    return(
+        <Fragment>
+            { isLoading ? <Spinner /> : products }
+        </Fragment>
+    )
+}
+
+export default ProductsSampleList;
+
---
+++ src/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from "react-router-dom";
+import { store, persistor } from './store/store';
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.utils';
+
+import { PersistGate } from 'redux-persist/integration/react';
+
+import './index.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLInputElement);
+root.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor} >
+      <BrowserRouter>
+        <Elements stripe={stripePromise}>
+          <App />    
+        </Elements>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
