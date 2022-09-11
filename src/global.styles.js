a6cb09c01ffd8f74fac958eca3e64cc568d9038e
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-09-10 17:56:12 -0700

    mobile styles

--- src/global.styles.js
+++ src/global.styles.js
@@ -4,6 +4,10 @@ export const GlobalStyles = createGlobalStyle`
 body {
   padding: 20px 40px;
   font-family: 'Open Sans Condensed', sans-serif;
+
+  @media screen and (max-width: 800px){
+    padding: 10px;
+  }
 }
 
 * {
